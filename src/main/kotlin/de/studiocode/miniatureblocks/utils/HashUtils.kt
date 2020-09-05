a5b6d35f7eb84371e428862d3a5bf8c3a3dd5081
ByteZ1337 <0xbytez@gmail.com>
2020-09-05 12:03:56 +0200

    Revert createSha1Hash changes

--- src/main/kotlin/de/studiocode/miniatureblocks/utils/HashUtils.kt
+++ src/main/kotlin/de/studiocode/miniatureblocks/utils/HashUtils.kt
@@ -4,10 +4,17 @@ import java.io.File
 import java.security.MessageDigest
 
 object HashUtils {
-    
+
     fun createSha1Hash(file: File): ByteArray {
+        val inputStream = file.inputStream()
         val md = MessageDigest.getInstance("SHA1")
-        return md.digest(file.readBytes())
+        var len = -1
+        val buffer = ByteArray(4096)
+        while ({len = inputStream.read(buffer); len}() != -1) {
+            md.update(buffer, 0, len)
+        }
+        inputStream.close()
+        return md.digest()
     }
     
 }
