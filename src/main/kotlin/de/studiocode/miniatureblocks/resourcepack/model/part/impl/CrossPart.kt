5c6e879ff5d65f97eda79c35252b0af76a156f08
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2021-03-02 16:19:08 +0100

    Removed unused textures from CrossPart

--- src/main/kotlin/de/studiocode/miniatureblocks/resourcepack/model/part/impl/CrossPart.kt
+++ src/main/kotlin/de/studiocode/miniatureblocks/resourcepack/model/part/impl/CrossPart.kt
@@ -10,13 +10,16 @@ import org.bukkit.Axis
 
 class CrossPart(data: ThreadSafeBlockData) : Part() {
     
-    private val texture = Texture(Texture.UV(0.0, 0.0, 1.0, 1.0), BlockTexture.of(data.material).textures[0])
+    private val blockTexture = BlockTexture.of(data.material)
     
     override val elements = listOf(CrossElement1(), CrossElement2())
     override val rotatable = false
     
     private inner class CrossElement1 :
-        Element(Point3D(0.078125, 0.0, 0.0), Point3D(1.328125, 1.0, 0.0), texture) {
+        Element(
+            Point3D(0.078125, 0.0, 0.0), Point3D(1.328125, 1.0, 0.0),
+            Texture(Texture.UV(0.0, 0.0, 1.0, 1.0), blockTexture.textures[0])
+        ) {
         
         init {
             setRotation(-45f, Axis.Y, 0.0, 0.0, 0.0)
@@ -24,7 +27,10 @@ class CrossPart(data: ThreadSafeBlockData) : Part() {
     }
     
     private inner class CrossElement2 :
-        Element(Point3D(-0.328125, 0.0, 0.0), Point3D(0.921875, 1.0, 0.0), texture) {
+        Element(
+            Point3D(-0.328125, 0.0, 0.0), Point3D(0.921875, 1.0, 0.0),
+            Texture(Texture.UV(0.0, 0.0, 1.0, 1.0), blockTexture.textures[1])
+        ) {
         
         init {
             setRotation(45f, Axis.Y, 1.0, 0.0, 0.0)
