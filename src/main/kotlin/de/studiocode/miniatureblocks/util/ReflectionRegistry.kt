4c57591f5831c16ce625fad8de333c8b0d2db327
NichtStudioCode <51272202+NichtStudioCode@users.noreply.github.com>
2021-08-30 20:05:47 +0200

    Fixed NoSuchtMethodException on newer spigot versions

--- src/main/kotlin/de/studiocode/miniatureblocks/util/ReflectionRegistry.kt
+++ src/main/kotlin/de/studiocode/miniatureblocks/util/ReflectionRegistry.kt
@@ -14,7 +14,6 @@ import org.bukkit.Bukkit
 import org.bukkit.Location
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason
 import org.bukkit.inventory.ItemStack
-import org.bukkit.util.Consumer
 
 @Suppress("MemberVisibilityCanBePrivate", "UNCHECKED_CAST")
 object ReflectionRegistry {
@@ -47,7 +46,7 @@ object ReflectionRegistry {
     val CB_CRAFT_SERVER_SYNC_COMMANDS_METHOD = getMethod(CB_CRAFT_SERVER_CLASS, true, "syncCommands")
     val CB_CRAFT_ENTITY_GET_HANDLE_METHOD = getMethod(CB_CRAFT_ENTITY_CLASS, false, "getHandle")
     val CB_CRAFT_WORLD_CREATE_ENTITY_METHOD = getMethod(CB_CRAFT_WORLD_CLASS, false, "createEntity", Location::class.java, Class::class.java)
-    val CB_CRAFT_WORLD_ADD_ENTITY_METHOD = getMethod(CB_CRAFT_WORLD_CLASS, false, "addEntity", NMS_ENTITY_CLASS, SpawnReason::class.java, Consumer::class.java)
+    val CB_CRAFT_WORLD_ADD_ENTITY_METHOD = getMethod(CB_CRAFT_WORLD_CLASS, false, "addEntity", NMS_ENTITY_CLASS, SpawnReason::class.java)
     val CB_CRAFT_ITEM_STACK_AS_NMS_COPY_METHOD = getMethod(CB_CRAFT_ITEM_STACK_CLASS, false, "asNMSCopy", ItemStack::class.java)
     
     // NMS fields
